eb6530d854d59e4f8e8c5f00e7205b065fcbb7ce
vdsr <valleria@gmail.com>
2024-10-22 10:29:20 +0200

    Saving Blocks To Chain

--- block.mjs
+++ block.mjs
@@ -15,19 +15,10 @@ class Block{
     addTransaction(transaction){
         this.transactions.push(transaction);
     }
+
+    getTransactions(){
+        console.log(this.transactions)
+    }
 }
 
 export default Block;
-
-const block = Block.createBlock(1, 123, "gsdgquigwe", "yusdgjha");
-const block2 = Block.createBlock(2, 456, "uwghuiebc", block.hash)
-
-const transaction = {
-    amount : 1,
-    sender : "Alex",
-    receiver : "Anna"
-};
-
-block.addTransaction(transaction);
-
-console.log(block, block2);
--- blockchain.mjs
+++ blockchain.mjs
@@ -1,3 +1,5 @@
+import Block from "./block.mjs";
+
 class Blockchain{
     constructor(){
         this.chain = [];
@@ -6,10 +8,31 @@ class Blockchain{
     getChain(){
         console.log(this.chain);
     }
+
+    addBlock(block){
+        this.chain.push(block)
+    }
+
 }
 
 export default Blockchain;
 
 const blockchain = new Blockchain();
 
+const block = Block.createBlock(1, 123, "gsdgquigwe", "yusdgjha");
+const block2 = Block.createBlock(2, 456, "uwghuiebc", block.hash)
+
+const transaction = {
+    amount : 1,
+    sender : "Alex",
+    receiver : "Anna"
+};
+
+block.addTransaction(transaction);
+
+
+blockchain.addBlock(block);
+blockchain.addBlock(block2);
+
 blockchain.getChain();
+block.getTransactions();
